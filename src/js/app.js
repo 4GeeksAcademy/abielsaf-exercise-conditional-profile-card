9146bcf07ebd633d347431c82ead3c69965c3bcd
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2023-12-04 20:09:31 +0000

    Working as expected, need to remove null from html

--- src/js/app.js
+++ src/js/app.js
@@ -32,16 +32,38 @@ function render(variables = {}) {
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
+
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
+
+          
+          <h1>${variables.name ? variables.name : "Name"} ${
+    variables.lastName ? variables.lastName : "Last Name"
+  } </h1>
+
+          <h2>${variables.role ? variables.role : "Put your Role"}</h2>
+
+
+          <h3>${variables.country ? variables.country : "Country"} ${
+    variables.city ? variables.city : "City"
+  }</h3>
+  <ul class="${variables.socialMediaPosition}">
+
+
+
+  <li><a href="https://twitter.com/${
+    variables.twitter ? variables.twitter : "RMBaloncesto"
+  }"><i class="fab fa-twitter"></i></a></li>
+  <li><a href="https://github.com/${
+    variables.github ? variables.github : "abielsaf"
+  }"><i class="fab fa-github"></i></a></li>
+  <li><a href="https://linkedin.com/in/${
+    variables.linkedin
+  }"><i class="fab fa-linkedin"></i></a></li>
+  <li><a href="https://instagram.com/${
+    variables.instagram ? variables.instagram : "realmadrid"
+  }"><i class="fab fa-instagram"></i></a></li>
+</ul>
+</div>
         </div>
     `;
 }
